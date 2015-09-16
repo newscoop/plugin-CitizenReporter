38de14265062df403bd4c6510fd31bfeed3ce541
Rafał Muszyński <rmuszynski1@gmail.com>
2015-09-16 21:37:32 +0200

    init bundle schema
    set up plugin
    
    remove translations folder

---
+++ NewscoopCitizenReporterPluginBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2015 Sourcefabric z.ú.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+namespace Newscoop\CitizenReporterPluginBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewscoopCitizenReporterPluginBundle extends Bundle
+{
+}
